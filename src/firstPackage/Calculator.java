c9f8022e862fb72ac1fa1a80086e98387c466030
= <=>
2021-08-04 17:06:15 -0500

    first commit

---
+++ src/firstPackage/Calculator.java
@@ -0,0 +1,19 @@
+package firstPackage;
+
+public class Calculator {
+  public static void main(String[] args) {
+
+  }
+  static int x=5;
+  static int y=20;
+
+  static int addition= x+y;
+
+    static int subtraction= x-y;
+    static int multiplication= x*y;
+    static int division=x/y;
+
+
+
+
+}
